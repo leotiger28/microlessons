51bee6a98ace74b8e05da2aa130691f8f2edc217
Лев Максимов <lmaksimov@diasoft.ru>
2021-04-15 20:26:05 +0300

    10. Поддержка событийно-ориентированной архитектуры ч2

--- lesson10/src/main/java/ru/leo/springdemo/events/MessageListener.java
+++ lesson10/src/main/java/ru/leo/springdemo/events/MessageListener.java
@@ -2,11 +2,16 @@ package ru.leo.springdemo.events;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.context.annotation.Configuration;
+import ru.leo.springdemo.entity.Greeting;
+import ru.leo.springdemo.service.GreetingServiceImpl;
 
 @Configuration
 public class MessageListener {
+    @Autowired
+    GreetingServiceImpl service;
 
     private Logger logger = LogManager.getLogger(MessageListener.class);
 
@@ -15,11 +20,15 @@ public class MessageListener {
     @StreamListener(ConsumerChannels.DIRECTED)
     public void directed(String message) {
         logger.debug("Сообщение получателю : " + message);
+        Greeting greeting = new Greeting(message);
+        service.createGreeting(greeting);
     }
 
     @StreamListener(ConsumerChannels.BROADCASTS)
     public void broadcasted(String message) {
         logger.debug("Сообщение всем : " + message);
+        Greeting greeting = new Greeting(message);
+        service.createGreeting(greeting);
     }
 }
 
